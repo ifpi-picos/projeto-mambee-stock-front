7b55c616b5d48b3290d0fcd5dd4a477ef0f33447
jackonPrimo <jackson.primo12@gmail.com>
2020-12-25 10:57:55 -0300

    remove signup page

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SigninModule } from './signin/signin.module';
-import { SignupModule } from './signup/signup.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, SigninModule, SignupModule],
+  imports: [CommonModule, SigninModule],
 })
 export class HomeModule {}
