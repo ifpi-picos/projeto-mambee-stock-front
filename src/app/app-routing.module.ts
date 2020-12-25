719afaa6560f272f156b68de7c359c22c2ea1fb0
jackonPrimo <jackson.primo12@gmail.com>
2020-12-25 10:58:57 -0300

    added protecion in route to users not logged

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthguardService } from './core/services/authguard.service';
 import { SigninComponent } from './pages/home/signin/signin.component';
-import { SignupComponent } from './pages/home/signup/signup.component';
 import { ItemEditComponent } from './pages/item-edit/item-edit.component';
 import { ItensRegistrationComponent } from './pages/itens-registration/itens-registration.component';
 import { ItensComponent } from './pages/itens/itens.component';
@@ -12,12 +12,11 @@ import { UsersComponent } from './pages/users/users.component';
 const routes: Routes = [
   { path: '', redirectTo: 'signin', pathMatch: 'full' },
   { path: 'signin', component: SigninComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'itens-registration', component: ItensRegistrationComponent },
-  { path: 'itens', component: ItensComponent },
-  { path: 'item-edit', component: ItemEditComponent },
-  { path: 'user-registration', component: UserRegistrationComponent },
-  { path: 'users', component: UsersComponent }
+  { path: 'itens-registration', component: ItensRegistrationComponent, canActivate: [AuthguardService] },
+  { path: 'itens', component: ItensComponent, canActivate: [AuthguardService] },
+  { path: 'item-edit', component: ItemEditComponent, canActivate: [AuthguardService] },
+  { path: 'user-registration', component: UserRegistrationComponent, canActivate: [AuthguardService] },
+  { path: 'users', component: UsersComponent, canActivate: [AuthguardService] }
 ];
 
 @NgModule({
---
+++ src/app/core/services/authguard.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {take, map, tap} from 'rxjs/operators'
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthguardService implements CanActivate{
+  constructor(private afAuth: AngularFireAuth, private router: Router) { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.afAuth.authState.pipe(
+      take(1),
+      map(user => !!user),
+      tap(loggedIn => {
+        if (!loggedIn) {
+          this.router.navigate(['/']);
+        }
+      })
+    )
+  }
+}
