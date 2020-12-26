91e0ee5ef984c058ec61167adb9f79bf35fc9f66
jackonPrimo <jackson.primo12@gmail.com>
2020-12-26 10:16:46 -0300

    added config to firebase hosting

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+      apiKey: "AIzaSyCNUx7jMCy-WLn4WEcb4rUuDpfIpzhOiDY",
+      authDomain: "mambee-stock-eeec6.firebaseapp.com",
+      projectId: "mambee-stock-eeec6",
+      storageBucket: "mambee-stock-eeec6.appspot.com",
+      messagingSenderId: "734938748049",
+      appId: "1:734938748049:web:eeea40ad4e75cd4e03db30"
+    },
+  };
