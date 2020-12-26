ce9e9ce09fc2e5cc883f75536bf53eaa21a0ced8
jackonPrimo <jackson.primo12@gmail.com>
2020-12-26 08:20:44 -0300

    add routes in menu

--- src/app/shared/components/default/default.component.ts
+++ src/app/shared/components/default/default.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/auth/auth.service';
+import { UserService } from 'src/app/core/models/user/user.service';
 
 @Component({
   selector: 'default',
@@ -8,10 +9,20 @@ import { AuthService } from 'src/app/core/auth/auth.service';
   styleUrls: ['./default.component.css']
 })
 export class DefaultComponent implements OnInit {
-
+  public userRole: string;
   @Input() innerApp: boolean;
-  constructor(private auth: AuthService,public router: Router){}
-  ngOnInit(): void {}
+  constructor(
+    private auth: AuthService,
+    public router: Router,
+    private userService: UserService
+    ){}
+  ngOnInit(): void {
+    this.auth.authUser().subscribe(user=>{
+      this.userService.get(user.uid).subscribe(doc=>{
+        this.userRole = doc.role
+      })
+    })
+  }
   hideMenu(){
     let menu = document.getElementById("sidenav")
     menu.style.width = '0px'
